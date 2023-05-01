0a9cd0e45975bddb1522b1f70eeeb6ae341bee35
Julie Gunawan <julie.gunawan@protonmail.com>
2023-05-01 06:37:53 -0600

    adding build

---
+++ build/precache-manifest.018eea4062aea86988918efa06760277.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "352b85214863484aac48",
+    "url": "/static/css/main.3d42a946.chunk.css"
+  },
+  {
+    "revision": "352b85214863484aac48",
+    "url": "/static/js/main.352b8521.chunk.js"
+  },
+  {
+    "revision": "988451d658de450458cd",
+    "url": "/static/js/1.988451d6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ec597daf264afc6c6a219598afa912ff",
+    "url": "/index.html"
+  }
+];
